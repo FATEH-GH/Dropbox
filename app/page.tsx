a83f8c5bf020aa021cc044b8494f3c1abba40682
FATEH-GH <fatehghribi27@gmail.com>
2024-01-23 11:50:44 +0100

    added animations to the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,67 @@
 import { ArrowRight } from "lucide-react";
-import Link from "next/link";
+
+import { motion } from "framer-motion";
+import {
+  MotionDiv,
+  MotionH1,
+  MotionLink,
+  MotionP,
+} from "@/components/FramerMotion";
+
+const container = {
+  hidden: { y: 10, opacity: 0 },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const item = {
+  hidden: { y: 10, opacity: 0 },
+  show: { y: 0, opacity: 1 },
+};
 
 export default function Home() {
   return (
-    <main className="">
+    <main>
       <div className="flex flex-col lg:flex-row items-center bg-gray-600 dark:bg-slate-800 ">
-        <div className="p-10 flex flex-col bg-gray-600 dark:bg-slate-800 text-white">
-          <h1 className="text-3xl sm:text-5xl font-bold ">
+        <MotionDiv
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="p-10 flex flex-col bg-gray-600 dark:bg-slate-800 text-white"
+        >
+          <MotionH1 variants={item} className="text-3xl sm:text-5xl font-bold">
             Welcome to Dropbox.
             <br />
             <br />
             Storing everything for you and your buisness needs. All in one place
-          </h1>
-          <p className="pb-20">
+          </MotionH1>
+          <MotionP variants={item} className="py-10">
             Enhance your personal storage with Dropbox, offering a simple and
             efficient way to upload, organize and access files from anywhere.
             Securely store inportant documents and media, and experience the
             convenience of easi file management and sharing in one centralized
             solution
-          </p>
-          <Link
+          </MotionP>
+          <MotionLink
+            variants={item}
             href="/dashboard"
-            className="flex cursor-pointer bg-blue-500 p-5 w-fit"
+            className="flex cursor-pointer font-bold bg-blue-500 p-5 w-fit group border rounded-xl"
           >
             Try it for free!
-            <ArrowRight className="ml-10" />
-          </Link>
-        </div>
-        <div className="bg-gray-600 dark:bg-slate-800 h-full p-10">
+            <ArrowRight className="ml-10 group-hover:translate-x-2 duration-200" />
+          </MotionLink>
+        </MotionDiv>
+        <MotionDiv
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+          className="bg-gray-600 dark:bg-slate-800 h-full p-10"
+        >
           <video autoPlay loop muted className="rounded-lg">
             <source
               src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/dropbox/dbx1-hero-1920x1080.mp4"
@@ -35,9 +69,13 @@ export default function Home() {
             />
             Your browser does not support the video tag
           </video>
-        </div>
+        </MotionDiv>
       </div>
-      <div className="">
+      <MotionDiv
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 1.2, ease: "easeOut" }}
+      >
         <p className="text-center font-bold text-xl pt-5">Disclaimer</p>
         <p className="text-center font-light p-2">
           this website is made for informational and educational purposes only.
@@ -46,7 +84,7 @@ export default function Home() {
           Act 1976, allowance is made for &quot;fair use&quot; of this website
           for showcasing skills of building nice websites
         </p>
-      </div>
+      </MotionDiv>
     </main>
   );
 }
---
+++ components/FramerMotion.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+export const MotionDiv = motion.div;
+export const MotionP = motion.p;
+export const MotionH1 = motion.h1;
+export const MotionLink = motion(Link);
